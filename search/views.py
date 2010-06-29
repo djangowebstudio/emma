9335dbb4b9ec73eb930bb9e0b218ebec41585f7b
geert <geert@6fed6ac5-cd60-4bc6-825a-a7cb102aa215>
2010-06-29 13:43:32 +0000

    added spelling suggestion to haystack search

--- search/views.py
+++ search/views.py
@@ -2,14 +2,15 @@ from haystack.views import SearchView
 from haystack.query import SearchQuerySet
 from django.contrib.auth.decorators import login_required
 
-
 class EmmaSearchView(SearchView):
-    def __init__(self):
-        self.req = req
+    def __name__(self):
+        return "EmmaSearchView"
     def extra_context(self):
-        c = SearchQuerySet().filter(content=self.req).count()
-        return c
+        sqs = super(EmmaSearchView, self).extra_context()
+        sqs['sqs'] = SearchQuerySet().auto_query(self.query).spelling_suggestion()
+        return sqs
+
 
 @login_required
 def search(req):
-    return SearchView(template='search/search.html')(req)
+    return EmmaSearchView(template='search/search.html')(req)
