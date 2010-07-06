28ac73b837b5be07049ae2fcb5b9fc202be340c2
geert <geert@6fed6ac5-cd60-4bc6-825a-a7cb102aa215>
2010-07-06 22:28:45 +0000

    Removed referemce to copyright form search_indexes.py

--- interface/search_indexes.py
+++ interface/search_indexes.py
@@ -5,7 +5,6 @@ from models import Keyword, Metadata
 class MetadataIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     image_LNID = indexes.CharField(model_attr='image_LNID')
-    copyright = indexes.CharField(model_attr='copyright')
 
 site.register(Metadata, MetadataIndex)
         
