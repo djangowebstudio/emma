94149ce95a519f1c4a00980d6f375f7b0111f6d3
geert <geert@6fed6ac5-cd60-4bc6-825a-a7cb102aa215>
2010-07-06 17:37:12 +0000

    Added copyright search to search templates, removed keyword model rom haystack indexes & templates

--- interface/search_indexes.py
+++ interface/search_indexes.py
@@ -5,11 +5,7 @@ from models import Keyword, Metadata
 class MetadataIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     image_LNID = indexes.CharField(model_attr='image_LNID')
+    copyright = indexes.CharField(model_attr='copyright')
 
 site.register(Metadata, MetadataIndex)
-
-class KeywordIndex(indexes.SearchIndex):
-    text = indexes.CharField(document=True, use_template=True)
-    image_LNID = indexes.CharField(model_attr='image_LNID')
-site.register(Keyword, KeywordIndex)
         
--- search/views.py
+++ search/views.py
@@ -1,6 +1,9 @@
 from haystack.views import SearchView
 from haystack.query import SearchQuerySet
+from haystack.forms import SearchForm
 from django.contrib.auth.decorators import login_required
+from emma.interface.models import Metadata, Keyword
+from django.http import Http404
 
 class EmmaSearchView(SearchView):
     def __name__(self):
@@ -8,9 +11,17 @@ class EmmaSearchView(SearchView):
     def extra_context(self):
         sqs = super(EmmaSearchView, self).extra_context()
         sqs['sqs'] = SearchQuerySet().auto_query(self.query).spelling_suggestion()
+        try:
+            sqs['mlt'] = Metadata.objects.filter(keywords__contains=self.query)[0]
+        except:
+            sqs['mlt'] = ''
+        
         return sqs
 
 
 @login_required
 def search(req):
-    return EmmaSearchView(template='search/search.html')(req)
+    try:
+        return EmmaSearchView(template='search/search.html', form_class=SearchForm)(req)
+    except Exception, inst:
+        raise Http404
