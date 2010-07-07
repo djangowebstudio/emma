3540fb543554188d9783216b2296ff18acf51d3c
geert <geert@6fed6ac5-cd60-4bc6-825a-a7cb102aa215>
2010-07-07 06:15:20 +0000

    Corrected field name for copyright

--- interface/search_indexes.py
+++ interface/search_indexes.py
@@ -5,6 +5,7 @@ from models import Keyword, Metadata
 class MetadataIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     image_LNID = indexes.CharField(model_attr='image_LNID')
+    copyright = indexes.BooleanField(model_attr='copyright', null=True)
 
 site.register(Metadata, MetadataIndex)
         
