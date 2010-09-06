eee9e42f724702b3b1876bb796364945f61097e3
geert <geert@6fed6ac5-cd60-4bc6-825a-a7cb102aa215>
2010-09-06 17:07:25 +0000

    added feedback module to emma tree, added python 2.5 support to emma.feedback

--- cart/urls.py
+++ cart/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
-try: 
-    import json
-except:
-    import simplejson as json
 
 urlpatterns = patterns('emma.cart.views',
             url(r'^add/item/(?P<item>.*)/$', 'add', name='add_item'),
---
+++ feedback/views.py
@@ -0,0 +1,34 @@
+# Create your views here.
+from django.contrib.sites.models import Site
+from django.http import HttpResponse
+from django.utils.encoding import force_unicode
+
+try:
+    import json
+except:
+    import simplejson as json
+    
+import forms
+
+def sanitize(errors):
+    dct = dict((str(k),list(force_unicode(a) for a in v)) for k,v in errors.items())
+    return dct
+
+def handle_ajax(request, url):
+    if not request.POST:
+        return HttpResponse(json.dumps({'error':'no post recieved'}))
+    else:
+        post = {}
+        for k in request.POST:
+            post[k] = request.POST[k]
+        post['url'] = url
+        post['site'] = Site.objects.get_current().id
+        form = forms.FeedbackForm(post)
+        if form.is_valid():
+            form.save()
+            return HttpResponse(json.dumps({}))
+        else:
+            
+            return HttpResponse(json.dumps({'errors':sanitize(form.errors)}))
+
+
