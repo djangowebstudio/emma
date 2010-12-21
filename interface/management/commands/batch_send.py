6324b0d0aa3ad87e5ecc9ea00394526daa670b84
geert <geert@6fed6ac5-cd60-4bc6-825a-a7cb102aa215>
2010-12-21 08:02:48 +0000

    add test email

--- interface/management/commands/batch_send.py
+++ interface/management/commands/batch_send.py
@@ -17,36 +17,75 @@ class Command(BaseCommand):
                 dest='action',
                 default=False, 
                 help='Do the action.'),
+                
+            make_option('-t', '--template', 
+                action='store', 
+                dest='template',
+                default='', 
+                help='Enter template name (without the extension)'),
+                
+            make_option('-s', '--subject', 
+                action='store', 
+                dest='subject',
+                default='', 
+                help='Enter the email subject line'),
+                
+            make_option('-e', '--email', 
+                action='store', 
+                dest='email',
+                default='', 
+                help='Enter an email address for testing purposes'),
+                 
             )
             
-    def handle(self, template='', subject='', *args, **options):
+    def handle(self, *args, **options):
         action = options.get('action', False)
+        template = options.get('template', '')
+        subject = options.get('subject', '')
+        email = options.get('email', '')
         if not action: 
             print "add option -r (or --for-real) to execute."
         if not template:
-            print "Please add template as first argument."
+            print "Please add  template argument. (-t, --template)"
             return None
         if not subject:
-            print "Please add subject as second argument."
+            print "Please add subject argument. (-s, --subject)"
             return None
         print 'with emailtemplate %s' % template
         t = loader.get_template('emailtemplates/%s.html' % template)
         alt = loader.get_template('emailtemplates/%s.txt' % template)
-        users = User.objects.all()
-        for user in users:
-            if user.is_active:
-                if user.email:
-                    print '%s %s %s' % (user.first_name, user.last_name, user.email)
-                    c = Context({'name': user.first_name})
-                    subject, from_email, to = subject, 'beeldnet@schepper.nl', user.email
-                    text_content = alt.render(c)
-                    html_content = t.render(c)
-                    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-                    msg.attach_alternative(html_content, "text/html")
-                    if action:
-                        try:
-                            msg.send()
-                        except Exception, inst:
-                            print 'An error occurred %s' % inst
+        if not email:
+            users = User.objects.all()
+            for user in users:
+                if user.is_active:
+                    if user.email:
+                        print '%s %s %s' % (user.first_name, user.last_name, user.email)
+                        c = Context({'name': user.first_name})
+                        subject, from_email, to = subject, 'beeldnet@schepper.nl', user.email
+                        text_content = alt.render(c)
+                        html_content = t.render(c)
+                        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+                        msg.attach_alternative(html_content, "text/html")
+                        if action:
+                            try:
+                                msg.send()
+                                print 'Successfully sent email to %s.' % user.email
+                            except Exception, inst:
+                                print 'An error occurred %s' % inst
+        else:
+            print 'Send test email to %s.' % email
+            c = Context({'name': 'Admin'})
+            subject, from_email, to = subject, 'beeldnet@schepper.nl', email
+            text_content = alt.render(c)
+            html_content = t.render(c)
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            if action:
+                try:
+                    msg.send()
+                    print 'Successfully sent email to %s.' % email
+                except Exception, inst:
+                    print 'An error occurred %s' % inst
+            
     
     
