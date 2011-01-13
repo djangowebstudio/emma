859819b71ad3569ab52bd6d65367dd8ff5bb75ea
geert <geert@6fed6ac5-cd60-4bc6-825a-a7cb102aa215>
2011-01-13 13:29:22 +0000

    remove tabs

--- interface/admin.py
+++ interface/admin.py
@@ -28,78 +28,78 @@ class FlatPageAdmin(FlatPageAdmin):
 # We have to unregister it, and then reregister
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatPageAdmin)
-	  	
+        
 class ImageAdmin(admin.ModelAdmin):
-	list_display = ['image_LNID', 'image_category']
-	search_fields = ['image_LNID', 'image_category']
-	
+    list_display = ['image_LNID', 'image_category']
+    search_fields = ['image_LNID', 'image_category']
+    
 admin.site.register(Image, ImageAdmin)
-	
+    
 class OrderAdmin(admin.ModelAdmin):
-	list_display = ('image_LNID', 'client', 'project', 'is_published', 'status', 'album_identifier', 'ts')
-	search_fields = ['client']
-	
+    list_display = ('image_LNID', 'client', 'project', 'is_published', 'status', 'album_identifier', 'ts')
+    search_fields = ['client']
+    
 admin.site.register(Order, OrderAdmin)
-	
+    
 class KeywordAdmin(admin.ModelAdmin):
-	list_display = ['image_LNID', 'subject', 'keywords', 'source']
-	search_fields = ['image_LNID', 'subject', 'keywords', 'source']
+    list_display = ['image_LNID', 'subject', 'keywords', 'source']
+    search_fields = ['image_LNID', 'subject', 'keywords', 'source']
 
 admin.site.register(Keyword, KeywordAdmin)
 
 
 class MetadataAdmin(admin.ModelAdmin):
     save_on_top = True
-	search_fields = ['image_LNID', 'subject', 'album', 'mime_type']
-	fieldsets = (
-	('About you', {'fields': ('caption_writer',)}),
-	('Image information', {
-							'classes': ('collapse',),
-							'fields': ('subject', 'copyright', 'profile', 'keywords', 'description', 'instructions', 'source', 'location', 'city', 'provincestate', 'country', 'datetimeoriginal')}),
-							
-	('Attachments', { 'classes': ('collapse',),
-						'fields': ('document',)}),
-	('Author information', {
-							'classes': ('collapse',),
-							'fields':('author', 'creator', 'credit')}),
-	('Album information', {
-							'classes': ('collapse',),
-							'fields':('album', 'headline')})
-	)
-	
-	
-	list_display = ('thumb','image_LNID','subject','copyright','profile','album', 'headline', 'has_attachment', 'keywords', 'mime_type')
-	
+    search_fields = ['image_LNID', 'subject', 'album', 'mime_type']
+    fieldsets = (
+    ('About you', {'fields': ('caption_writer',)}),
+    ('Image information', {
+                            'classes': ('collapse',),
+                            'fields': ('subject', 'copyright', 'profile', 'keywords', 'description', 'instructions', 'source', 'location', 'city', 'provincestate', 'country', 'datetimeoriginal')}),
+                            
+    ('Attachments', { 'classes': ('collapse',),
+                        'fields': ('document',)}),
+    ('Author information', {
+                            'classes': ('collapse',),
+                            'fields':('author', 'creator', 'credit')}),
+    ('Album information', {
+                            'classes': ('collapse',),
+                            'fields':('album', 'headline')})
+    )
+    
+    
+    list_display = ('thumb','image_LNID','subject','copyright','profile','album', 'headline', 'has_attachment', 'keywords', 'mime_type')
+    
 
 admin.site.register(Metadata, MetadataAdmin)
 
 class KeywordCountAdmin(admin.ModelAdmin):
-	search_fields = ['keyword']
+    search_fields = ['keyword']
 admin.site.register(KeywordCount, KeywordCountAdmin)
-			
+            
 class ContractAdmin(admin.ModelAdmin):
-	list_display = ('username', 'contract', 'date_signed')
-	search_fields = ['username']
-		
+    list_display = ('username', 'contract', 'date_signed')
+    search_fields = ['username']
+        
 admin.site.register(Contract, ContractAdmin)
 admin.site.register(Favorite)
 
 class AlbumAdmin(admin.ModelAdmin):
-	search_fields = ['album_name', 'album_identifier']
-	fieldsets = (
-	
-	('Album information', {
-			'fields': ('album_name',)}),
-	('Attachments', {
-			'fields': ('document', )},),
-			
-	('Items', {'classes': ('collapse',), 'fields': ('image',)})
-	
-	)
-	
-	filter_horizontal = ['image']
-	list_display = ('album_name', 'album_identifier', 'document')
-	
+    search_fields = ['album_name', 'album_identifier']
+    fieldsets = (
+    
+    ('Album information', {
+            'fields': ('album_name',)}),
+    ('Attachments', {
+            'fields': ('document', )},),
+            
+    ('Items', {'classes': ('collapse',), 'fields': ('image',)})
+    
+    )
+    
+    filter_horizontal = ['image']
+    list_display = ('album_name', 'album_identifier', 'document')
+    
 admin.site.register(Album, AlbumAdmin)
 class UserAdmin(admin.ModelAdmin):
     exlude = ['setstr2']
