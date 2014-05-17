77c26e88111e6bd78b147db8fdccaf2bbc002e81
Geert Dekkers <geert@djangowebstudio.nl>
2014-05-17 22:47:17 +0200

    up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='emma',
-    version=1.01,
+    version=1.02,
     description='Image Databank for Django',
     long_description=open('README.rst').read(),
     author='Geert Dekkers',
