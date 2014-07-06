abf45601965bdaf0c74b6eba049464d866d94f01
Geert Dekkers <geert@djangowebstudio.nl>
2014-07-06 20:49:32 +0200

    change package name because already taken

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='emma',
+    name='django_emma',
     version=1.02,
     description='Image Databank for Django',
     long_description=open('README.rst').read(),
